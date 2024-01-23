b01ecdf772cbb93d4d7bc73fb371cc32c6f95b21
Conor Judge <conorjudgetech@icloud.com>
2024-01-23 10:37:21 +0000

    Test

---
+++ src/contacts/BusinessContacts.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package contacts;
+
+/**
+ *
+ * @author work
+ */
+public class BusinessContacts extends Contacts{
+    protected String company;
+
+    public BusinessContacts() {
+    }
+    
+    
+    
+}
---
+++ src/contacts/Contacts.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package contacts;
+
+/**
+ *
+ * @author work
+ */
+public class Contacts {
+    protected String firstName, surname, email, phone, address;
+
+    public Contacts() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surName) {
+        this.surname = surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
+
